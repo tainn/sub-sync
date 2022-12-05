f6094ff5fc32a756674056bedf3a090624da8297
tainn <tainn@protonmail.com>
2022-12-06 00:08:34 +0100

    Minor source restructure

--- src/subsync.py
+++ src/subsync.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python3
 
-"""
-A script for setting offsets to subtitle files.
-
-An offset is passed in as a positional argument and is expressed in seconds:
-- negative: hasten (-0.500)
-- positive: delay (+0.800 or 0.800)
-
-On default, a .srt file located in the current directory is taken in.
-A condition for that to happen is that exactly 1 .srt file exists in it.
-
-To specify the exact file, a path keyword argument can be used:
---path: passes an absolute or relative path to the file.
-
-Upon running, the old subtitle file "foo" will be renamed to "foo-old" and the newly output file
-with the offsets in place will now be named "foo". The old file is not automatically deleted.
-"""
-
 import glob
-import logging
 import os
 import sys
 from argparse import ArgumentParser, Namespace
@@ -27,19 +9,15 @@ from datetime import timedelta as td
 
 def main() -> None:
     args: Namespace = parse_args()
-    subfile: str = get_file(args)
-    change_timelines(args, subfile)
-
-    logging.info("Complete!")
+    subsfile: str = get_file(args)
+    change_timelines(args, subsfile)
 
 
 def parse_args() -> Namespace:
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument("offset", help="amount of seconds to shift (+-0.000)", type=float)
     parser.add_argument("-p", "--path", help="absolute or relative path to the file")
-    args: Namespace = parser.parse_args()
-
-    return args
+    return parser.parse_args()
 
 
 def get_file(args: Namespace) -> str:
@@ -49,29 +27,30 @@ def get_file(args: Namespace) -> str:
     srts: set[str] = set(glob.glob("*.srt")) - set(glob.glob("*old-[0-9].srt"))
 
     if len(srts) == 0:
-        logging.error(
-            "No .srt file found\n"
-            "Position yourself inside a directory with the file or specify it via the --path flag"
-        )
+        print("No .srt file found", file=sys.stderr)
         sys.exit(1)
 
-    elif len(srts) != 1:
-        logging.error(
-            f"More than 1 .srt file found: {len(srts)}\n" "Consider using the --path flag to specify precisely one"
-        )
+    elif len(srts) > 1:
+        print(f"More than 1 .srt file found: {len(srts)}", file=sys.stderr)
         sys.exit(2)
 
     return srts.pop()
 
 
-def change_timelines(args: Namespace, subfile: str) -> None:
-    with open(subfile, "r", encoding="ISO-8859-1") as rf:
-        original_subfile: str = rf.read()
+def change_timelines(args: Namespace, subsfile: str) -> None:
+    with open(subsfile, "r", encoding="ISO-8859-1") as rf:
+        original_subsfile: str = rf.read().strip()
 
-    blocks: list[str] = original_subfile.split("\n\n")
+    blocks: list[str] = original_subsfile.split("\n\n")
     timelines: list[str] = [line.splitlines()[1] for line in blocks if len(line.splitlines()) >= 3]
 
-    raw_inits_and_ends: list[tuple[str, str]] = [(timeline.split()[0], timeline.split()[2]) for timeline in timelines]
+    raw_inits_and_ends: list[tuple[str, str]] = [
+        (
+            timeline.split()[0],
+            timeline.split()[2],
+        )
+        for timeline in timelines
+    ]
 
     parsed_inits_and_ends: list[tuple[td, td]] = [
         (
@@ -108,26 +87,27 @@ def change_timelines(args: Namespace, subfile: str) -> None:
         for init, end in altered_inits_and_ends
     ]
 
-    altered_subfile: str = original_subfile
+    altered_subsfile: str = original_subsfile
 
     for (raw_init, raw_end), (altered_init, altered_end) in zip(raw_inits_and_ends, formatted_altered_inits_and_ends):
-        altered_subfile: str = altered_subfile.replace(raw_init, altered_init).replace(raw_end, altered_end)
+        altered_subsfile: str = altered_subsfile.replace(raw_init, altered_init).replace(raw_end, altered_end)
 
-    old_subfile: str = subfile.replace(".srt", "-old-0.srt")
+    old_subsfile: str = subsfile.replace(".srt", "-old-0.srt")
     increment: int = 1
 
     while True:
-        if os.path.isfile(old_subfile):
-            old_subfile: str = old_subfile.replace(f"-old-{increment - 1}.srt", f"-old-{increment}.srt")
+        if os.path.isfile(old_subsfile):
+            old_subsfile: str = old_subsfile.replace(f"-old-{increment - 1}.srt", f"-old-{increment}.srt")
             increment += 1
             continue
 
-        os.rename(subfile, old_subfile)
+        os.rename(subsfile, old_subsfile)
         break
 
-    with open(subfile, "w", encoding="ISO-8859-1") as wf:
-        wf.write(altered_subfile)
+    with open(subsfile, "w", encoding="ISO-8859-1") as wf:
+        wf.write(altered_subsfile)
 
 
 if __name__ == "__main__":
     main()
+    print("Complete!")
